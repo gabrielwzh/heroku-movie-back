2c3f5b8eb7112267dde014d9a20cf016d34a2054
gabriel wong <gabrielwzh@gmail.com>
2020-11-18 16:25:28 +0800

    env var

--- db/database.js
+++ db/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-
+require('dotenv').config()
 
 mongoose
     // .connect('mongodb://localhost:27017/cinema', { useNewUrlParser: true })
-    .connect(process.env.MONGODB_URI, 
+    .connect(process.env.MONGODB_URI || 'mongodb://localhost/cinema', 
     { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(e => {
         console.error('Connection error', e.message)
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config() // .env
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -6,7 +7,7 @@ const db = require('./db/database')
 const movieRouter = require('./routes/movie-router')
 
 const app = express()
-const apiPort = process.env.PORT || 6000
+const apiPort = process.env.PORT || 5000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -14,9 +15,9 @@ app.use(bodyParser.json())
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+// }
 
 app.get('/', (req, res) => {
     res.send('Backend API server running!')
